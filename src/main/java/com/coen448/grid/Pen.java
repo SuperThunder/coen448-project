bb6df05ea610cfc18d695ebcb094e566979ea6fa
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2022-01-30 07:53:17 -0500

    Created Robot and Pen

---
+++ src/main/java/com/coen448/grid/Pen.java
@@ -0,0 +1,94 @@
+package com.coen448.grid;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+import java.util.HashMap;
+
+public class Pen {
+    protected enum State{
+        UP,
+        DOWN
+    }
+
+    protected enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    private State state;
+    private Integer[] currentPos;
+    private Direction currentDir;
+    private char arrow;
+
+    private BiMap<Integer, Direction> directionMap;
+    private HashMap<Pen.Direction, Character> directionArrows;
+
+
+    public Pen() {
+        initDirections();
+        state = State.UP;
+        currentDir = Direction.NORTH;
+        currentPos = new Integer[]{0, 0};
+        arrow = directionArrows.get(currentDir);
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Integer[] getCurrentPos() {
+        return currentPos;
+    }
+
+    public Direction getCurrentDir() {
+        return currentDir;
+    }
+
+    public void setCurrentDir(Direction currentDir) {
+        this.currentDir = currentDir;
+    }
+
+
+
+    public void setArrow(char arrow) {
+        this.arrow = arrow;
+    }
+
+    public BiMap<Integer, Direction> getDirectionMap() {
+        return directionMap;
+    }
+
+
+    public HashMap<Direction, Character> getDirectionArrows() {
+        return directionArrows;
+    }
+
+
+
+    private void initDirections(){
+        directionMap = HashBiMap.create();
+        directionMap.put(1, Pen.Direction.NORTH);
+        directionMap.put(2, Pen.Direction.EAST);
+        directionMap.put(3, Pen.Direction.SOUTH);
+        directionMap.put(4, Pen.Direction.WEST);
+
+        directionArrows = new HashMap<>();
+        directionArrows.put(Pen.Direction.NORTH, '\u2191');
+        directionArrows.put(Pen.Direction.EAST, '\u2192');
+        directionArrows.put(Pen.Direction.SOUTH, '\u2193');
+        directionArrows.put(Pen.Direction.WEST, '\u2190');
+    }
+
+    public void printPen(){
+        System.out.printf("Position: %d, %d - Pen: %s - Facing: %s \n",
+                currentPos[1], currentPos[0], state.name(), currentDir.name());
+    }
+
+}
